d1cf27e1f3e29a81cc34f3c065de2fa843c0ff9b
yamengZhen <3072807640@qq.com>
2021-03-14 10:30:34 +0800

    example01

---
+++ src/components/AlertDialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { State } from "@/store";
+import { computed, defineComponent, Ref, watch } from "vue";
+import { Store, useStore } from "vuex";
+export default defineComponent({
+  setup() {
+    const store: Store<State> = useStore();
+    const exception = computed(() => store.state.exception);
+    watch(exception, (newValue: Ref, o: Ref) => {
+      alert(newValue);
+    });
+  }
+});
+</script>
---
+++ src/views/example01/Example01.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
